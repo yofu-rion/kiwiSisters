587875a3ae877b560000f301a212f2ba04bef40c
yama-chiiii <nagayama0409@gmail.com>
2025-06-23 14:31:55 +0900

    ごめん

---
+++ controller/story/StoryPlayController1.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Kiwi+Maru&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="../../css/story/play1.css">
+</head>
+
+<body>
+    <div class="full">
+        <div class="kuuhaku">a</div>
+        <div class="comment">
+            <div class="hako">
+                <div class="name">なまえ</div>
+                <div class="text">ここに文章が入るよ</div>
+                <div class="menu">
+                    <a class="save">セーブ</a>
+                    <!-- <button>ロード</button> -->
+                   <a href="/kiwiSisters/controller/StartMenu.php" class="title">タイトル</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
