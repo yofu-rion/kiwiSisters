da92f161e4c4626b4f8f18ed7fb378350137fc8b
yama-chiiii <nagayama0409@gmail.com>
2025-07-24 00:21:14 +0900

    セーブ機能つくりたい4

--- controller/Save.php
+++ controller/Save.php
@@ -1,4 +1,9 @@
 <?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
 session_start();
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
