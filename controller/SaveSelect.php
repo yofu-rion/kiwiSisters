4104fc751f1bef7b55170f35589f6ece7e7ae7e2
yama-chiiii <nagayama0409@gmail.com>
2025-07-24 00:15:22 +0900

    セーブ機能つくりたい3

--- controller/SaveSelect.php
+++ controller/SaveSelect.php
@@ -1,10 +1,15 @@
 <?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
 session_start();
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-require '../vendor/autoload.php';
 use Hashids\Hashids;
+
 
 // ログイン確認
 if (!isset($_SESSION['login'])) {
